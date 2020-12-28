4540684566bbd3106938653de1cd8fb2fe155b19
hu dong lin <benabcd5779@gmail.com>
2020-12-28 22:41:28 +0800

    增加菜單頁面的路由

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//首頁路由//
 Route::get('/',[HomeController::class, 'index'])->name('home.index');
-Route::get('/home',[HomeController::class, 'login_index'])->name('home.login_index');
+Route::get('/home',[HomeController::class, 'login_index'])->middleware('auth')->name('home.login_index');
+//首頁路由//
 
+//身分驗證//
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+//身分驗證//
+
+
+//菜單//
+Route::get('/products', [ProductController::class, 'index'])->name('products.index'); //菜單
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show'); //菜單個別資訊
+Route::post('/products',[ProductController::class, 'store'])->middleware('auth')->name('products.store');//加入購物車
+//菜單//
+
+
+
