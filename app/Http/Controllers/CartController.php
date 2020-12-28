10fd946996b26a6bb7ebc9b75b37683e3289f11f
hu dong lin <benabcd5779@gmail.com>
2020-12-28 22:42:51 +0800

    增加CartController的方法

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function index()
+    {
+        $userid=auth()->user()->id;
+
+        $results = DB::table('carts')
+            ->join('products','carts.products_id','=','products.id')
+            ->join('users','carts.users_id', '=', 'users.id')
+            ->where('carts.users_id',$userid)
+            ->select('users.id','products.name','products.detail','products.price','carts.quantity')
+            ->get();
+
+        $data = [
+            'results' => $results,
+        ];
+        return view('carts.index',$data);
+
+    }
 }
