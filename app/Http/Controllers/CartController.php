1d8ad2242d760fdeba27983d75cb35383755c193
hu dong lin <benabcd5779@gmail.com>
2020-12-30 09:22:56 +0800

    新增CartController的方法

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
@@ -14,13 +16,25 @@ class CartController extends Controller
             ->join('products','carts.products_id','=','products.id')
             ->join('users','carts.users_id', '=', 'users.id')
             ->where('carts.users_id',$userid)
-            ->select('users.id','products.name','products.detail','products.price','carts.quantity')
+            ->select('users.id','products.name','products.detail','products.price','products.img2','carts.quantity')
             ->get();
 
         $data = [
             'results' => $results,
         ];
+
         return view('carts.index',$data);
 
     }
+
+
+    static public function cartItem() //顯示導覽列購物車內產品數量
+    {
+        if (isset(auth()->user()->id))
+        {
+            $userID = auth()->user()->id;
+            return Cart::where('users_id', $userID)->count();
+        }
+    }
+
 }
