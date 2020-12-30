b337dc13282201e9f95c5b111e052637161d60c6
hu dong lin <benabcd5779@gmail.com>
2020-12-30 23:21:24 +0800

    新增CartController的方法

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Orderlist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -55,10 +56,53 @@ class CartController extends Controller
     }
 
 
+
+
     public function destroy($id)
     {
         Cart::destroy($id);
         return redirect()->route('carts.index');
+    }
+
+
+    public function store(Request $request)
+    {
+        $userID = auth()->user()->id;
+
+        $totals = DB::table('carts')
+            ->join('products','carts.products_id','=','products.id')
+            ->where('carts.users_id',$userID)
+            ->select('products.name','products.price','carts.quantity')
+            ->get();
+
+        $count =  DB::table('carts')
+            ->join('products','carts.products_id','=','products.id')
+            ->where('carts.users_id',$userID)
+            ->select('products.name','products.price','carts.quantity')
+            ->count();
+
+        $total = 0;
+
+        for($i=0;$i<$count;$i++)
+        {
+            $price = $totals[$i]->price;
+            $quantity = $totals[$i]->quantity;
+            $total += $price * $quantity;
+        }
+        $allcarts = Cart::where('users_id',$userID)->get();
+        foreach ($allcarts as $carts)
+        {
+            $order = new Orderlist();
+            $order->users_id = $userID;
+            $order->products_id = $carts->products_id;
+            $order->total = $total;
+            $order->status = "準備中";
+            $order->method = "預定快取";
+            $order->save();
+            Cart::where('users_id',$userID)->delete();
+        }
+
+        return redirect()->route('products.index');
     }
 
     static public function cartItem() //顯示導覽列購物車內產品數量
