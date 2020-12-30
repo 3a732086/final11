f54bd358faef00cd0792e15abf05ee45b23e4cf7
hu dong lin <benabcd5779@gmail.com>
2020-12-30 20:38:47 +0800

    新增CartController的方法

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,7 +16,7 @@ class CartController extends Controller
             ->join('products','carts.products_id','=','products.id')
             ->join('users','carts.users_id', '=', 'users.id')
             ->where('carts.users_id',$userid)
-            ->select('users.id','products.name','products.detail','products.price','products.img2','carts.quantity')
+            ->select('carts.id','products.name','products.detail','products.price','products.img2','carts.quantity')
             ->get();
 
         $data = [
@@ -24,9 +24,42 @@ class CartController extends Controller
         ];
 
         return view('carts.index',$data);
-
     }
 
+    static public function total()
+    {
+        $userid=auth()->user()->id;
+
+        $totals = DB::table('carts')
+            ->join('products','carts.products_id','=','products.id')
+            ->where('carts.users_id',$userid)
+            ->select('products.name','products.price','carts.quantity')
+            ->get();
+
+        $count =  DB::table('carts')
+            ->join('products','carts.products_id','=','products.id')
+            ->where('carts.users_id',$userid)
+            ->select('products.name','products.price','carts.quantity')
+            ->count();
+
+        $total = 0;
+        for($i=0;$i<$count;$i++)
+        {
+            $price = $totals[$i]->price;
+            $quantity = $totals[$i]->quantity;
+
+            $total += $price * $quantity;
+        }
+
+        return $total;
+    }
+
+
+    public function destroy($id)
+    {
+        Cart::destroy($id);
+        return redirect()->route('carts.index');
+    }
 
     static public function cartItem() //顯示導覽列購物車內產品數量
     {
